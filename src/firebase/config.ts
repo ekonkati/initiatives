11fd39ceb8c6745363ad545cf6e171e6ca13fb54
ESHWAR KONKATI <eshwar.k@resustainability.com>
2025-11-22 18:37:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2492010116-b19fe",
+  "appId": "1:348410010934:web:e749ada156c614d0eee797",
+  "apiKey": "AIzaSyAJIqu7wBsqUBx-Z6mdYckrqRfUbDpK9fs",
+  "authDomain": "studio-2492010116-b19fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "348410010934"
+};
